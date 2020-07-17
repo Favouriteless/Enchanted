238a27d57d7769091fa33ecc88d53edebf4b1098
Favouriteless <fredricomathews@gmail.com>
2020-07-17 09:46:46 +0100

    Altar changes
    - Added a little to altar block class
    - Fixed the altar blockstate I broke

--- src/main/java/com/favouriteless/magicraft/blocks/Altar.java
+++ src/main/java/com/favouriteless/magicraft/blocks/Altar.java
@@ -4,18 +4,30 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockReader;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
+
+import javax.annotation.Nullable;
 
 public class Altar extends Block {
+
+    public static final BooleanProperty JOINED = BooleanProperty.create("joined"); // this is the property itself
 
     public Altar() {
         super(Block.Properties.create(Material.ROCK)
                 .hardnessAndResistance(5.0F, 6.0F)
                 .sound(SoundType.STONE));
     }
+
+    @Override
+    @Nullable
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return this.getDefaultState().with(JOINED, false);
+    }
+    /* I wont do logic for you but this is called when the block is placed and needs to return if it is joined or not.
+    it needs to detect a 2x3 area of altar blocks, if so return with JOINED true, else JOINED false
+     */
+
 
 
 }

477cd69ec1c44d5ce942f1b08e66000a6ff895fd
Favouriteless <62647254+Favouriteless@users.noreply.github.com>
2021-08-19 20:46:36 +0100

    - Glint Weed
    - Updated gradle
    - Glint weed temp texture
    - Glint weed placement fixed
    - Glint weed loot table
    - Glint weed spreading

--- src/main/java/com/favouriteless/enchanted/common/blocks/crops/GlintWeedBlock.java
+++ src/main/java/com/favouriteless/enchanted/common/blocks/crops/GlintWeedBlock.java
@@ -1,7 +1,29 @@
+/*
+ * Copyright (c) 2021. Favouriteless
+ * Enchanted, a minecraft mod.
+ * GNU GPLv3 License
+ *
+ *     This file is part of Enchanted.
+ *
+ *     Enchanted is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Enchanted is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Enchanted.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.favouriteless.enchanted.common.blocks.crops;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BushBlock;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorldReader;
 import net.minecraft.world.server.ServerWorld;
@@ -16,12 +38,27 @@ public class GlintWeedBlock extends BushBlock {
 
     @Override
     public boolean canSurvive(BlockState pState, IWorldReader pLevel, BlockPos pPos) {
-        return super.canSurvive(pState, pLevel, pPos);
+        for(Direction dir : Direction.values()) {
+            if(pLevel.getBlockState(pPos.offset(dir.getNormal()))
+                    .isFaceSturdy(pLevel, pPos.offset(dir.getNormal()), dir.getOpposite())) {
+                return true;
+            }
+        }
+        return false;
     }
 
+    @Override
     public void randomTick(BlockState pState, ServerWorld pLevel, BlockPos pPos, Random pRandom) {
-        if (pLevel.random.nextInt(4) == 0 && pLevel.isAreaLoaded(pPos, 4)) {
+        if (pLevel.random.nextInt(6) == 0 && pLevel.isAreaLoaded(pPos, 4)) {
+            BlockPos newPos = pPos.offset(new BlockPos(
+                    RANDOM.nextInt(7) - 3,
+                    RANDOM.nextInt(7) - 3,
+                    RANDOM.nextInt(7) - 3));
 
+            if(pLevel.isEmptyBlock(newPos) && pLevel.getBlockState(newPos).isFaceSturdy(pLevel, newPos, Direction.UP)) {
+                pLevel.setBlockAndUpdate(newPos, pState);
+            }
         }
     }
+
 }
--- src/main/java/com/favouriteless/enchanted/core/init/EnchantedBlocks.java
+++ src/main/java/com/favouriteless/enchanted/core/init/EnchantedBlocks.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2021. Favouriteless
+ * Enchanted, a minecraft mod.
+ * GNU GPLv3 License
+ *
+ *     This file is part of Enchanted.
+ *
+ *     Enchanted is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Enchanted is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Enchanted.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.favouriteless.enchanted.core.init;
 
 import com.favouriteless.enchanted.Enchanted;
@@ -69,7 +90,7 @@ public class EnchantedBlocks {
     public static final RegistryObject<Block> BELLADONNA = BLOCKS.register("belladonna", () -> new BelladonnaBlock(AbstractBlock.Properties.copy(Blocks.WHEAT)));
     public static final RegistryObject<Block> ARTICHOKE = BLOCKS.register("artichoke", () -> new ArtichokeBlock(AbstractBlock.Properties.copy(Blocks.WHEAT).sound(SoundType.LILY_PAD)));
     public static final RegistryObject<Block> MANDRAKE = BLOCKS.register("mandrake", () -> new MandrakeBlock(AbstractBlock.Properties.copy(Blocks.WHEAT)));
-    public static final RegistryObject<Block> GLINT_WEED = BLOCKS.register("glint_weed", () -> new GlintWeedBlock(AbstractBlock.Properties.copy(Blocks.POPPY)));
+    public static final RegistryObject<Block> GLINT_WEED = BLOCKS.register("glint_weed", () -> new GlintWeedBlock(AbstractBlock.Properties.copy(Blocks.POPPY).lightLevel((a) -> 14).randomTicks()));
 
     public static final RegistryObject<Block> CHALK_GOLD = BLOCKS.register("chalk_gold", GoldChalkBlock::new);
     public static final RegistryObject<Block> CHALK_WHITE = BLOCKS.register("chalk_white", () -> new ChalkCircleBlock(null));

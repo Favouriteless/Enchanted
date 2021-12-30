df345b6060d256f5caee7e5d3b7dc20110fcd81d
Favouriteless <62647254+Favouriteless@users.noreply.github.com>
2021-12-30 17:00:08 +0000

    Fixed a lot of stuff
    - Build no longer breaks (lol what?)
    - Kettle recipes and tools added to books

--- src/main/java/com/favouriteless/enchanted/api/patchouli/components/ItemListComponent.java
+++ src/main/java/com/favouriteless/enchanted/api/patchouli/components/ItemListComponent.java
@@ -27,6 +27,9 @@ import vazkii.patchouli.api.IComponentRenderContext;
 import vazkii.patchouli.api.ICustomComponent;
 import vazkii.patchouli.api.IVariable;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.UnaryOperator;
 
 public class ItemListComponent implements ICustomComponent {
@@ -35,32 +38,62 @@ public class ItemListComponent implements ICustomComponent {
 	public boolean centered;
 	public int itemsPerRow;
 	public IVariable itemList;
-	public transient ItemStack[] items;
+	public transient List<ItemStack> items;
 	private transient int x;
 	private transient int y;
+
+	private transient List<ItemRow> itemRows = new ArrayList<>();
 
 	@Override
 	public void build(int componentX, int componentY, int pageNum) {
 		this.x = componentX;
 		this.y = componentY;
+		repopulateRows();
 	}
 
 	@Override
 	public void render(MatrixStack matrix, IComponentRenderContext context, float partialticks, int mouseX, int mouseY) {
-		int itemsRendered = 0;
-		int width = Math.min(itemsPerRow, items.length)*16;
-		int xOffset = centered ? -(width/2): 0;
-		int yMult = startBottom ? -1 : 1;
-		int yOffset = startBottom ? -16 : 0;
-
-		for(ItemStack item : items) {
-			context.renderItemStack(matrix, x+((itemsRendered%itemsPerRow) * 16)+xOffset, y+((itemsRendered/itemsPerRow) * 16)*yMult+yOffset, mouseX, mouseY, item);
-			itemsRendered++;
+		for(ItemRow row : itemRows) {
+			row.render(matrix, context, mouseX, mouseY);
 		}
 	}
 
 	@Override
 	public void onVariablesAvailable(UnaryOperator<IVariable> lookup) {
-		items = lookup.apply(itemList).as(ItemStack[].class);
+		items = Arrays.asList(lookup.apply(itemList).as(ItemStack[].class));
+	}
+
+	public void repopulateRows() {
+		this.itemRows.clear();
+
+		int startY = startBottom ? y - ((int)Math.ceil(items.size()/(float)itemsPerRow))*16 : y;
+		int index = 0;
+		while(index < items.size()) {
+			List<ItemStack> subList = items.subList(index, Math.min(items.size(), index+itemsPerRow));
+			itemRows.add(new ItemRow(subList, x, startY + (index/itemsPerRow)*16, centered));
+
+			index += itemsPerRow;
+		}
+	}
+
+	private static class ItemRow {
+
+		private final List<ItemStack> items;
+		private final int x;
+		private final int y;
+
+		private ItemRow(List<ItemStack> items, int x, int y, boolean centered) {
+			this.items = items;
+			this.x = centered ? Math.round(x - items.size()*16/2.0F) : x;
+			this.y = y;
+		}
+
+		private void render(MatrixStack matrix, IComponentRenderContext context, int mouseX, int mouseY) {
+			int xOffset = 0;
+			for(ItemStack stack : items) {
+				context.renderItemStack(matrix, x+xOffset, y, mouseX, mouseY, stack);
+				xOffset += 16;
+			}
+		}
 	}
 }

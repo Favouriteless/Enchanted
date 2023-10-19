9585e9545b61e13a3fa4e6bdc384351c3ce0940d
Favouriteless <62647254+Favouriteless@users.noreply.github.com>
2023-10-19 20:41:16 +0100

    FamiliarType registry

--- common/src/main/java/com/favouriteless/enchanted/common/init/registry/FamiliarTypes.java
+++ common/src/main/java/com/favouriteless/enchanted/common/init/registry/FamiliarTypes.java
@@ -1,54 +1,43 @@
-/*
- *
- *   Copyright (c) 2023. Favouriteless
- *   Enchanted, a minecraft mod.
- *   GNU GPLv3 License
- *
- *       This file is part of Enchanted.
- *
- *       Enchanted is free software: you can redistribute it and/or modify
- *       it under the terms of the GNU General Public License as published by
- *       the Free Software Foundation, either version 3 of the License, or
- *       (at your option) any later version.
- *
- *       Enchanted is distributed in the hope that it will be useful,
- *       but WITHOUT ANY WARRANTY; without even the implied warranty of
- *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *       GNU General Public License for more details.
- *
- *       You should have received a copy of the GNU General Public License
- *       along with Enchanted.  If not, see <https://www.gnu.org/licenses/>.
- *
- *
- */
-
 package com.favouriteless.enchanted.common.init.registry;
 
 import com.favouriteless.enchanted.Enchanted;
 import com.favouriteless.enchanted.api.familiars.FamiliarType;
 import com.favouriteless.enchanted.common.familiars.types.CatFamiliarType;
-import com.favouriteless.enchanted.core.util.RegistryHelper.Generify;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.IForgeRegistry;
-import net.minecraftforge.registries.RegistryBuilder;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.entity.TamableAnimal;
 
-import java.util.function.Supplier;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FamiliarTypes {
 
-	public static final DeferredRegister<FamiliarType<?, ?>> FAMILIAR_TYPES = DeferredRegister.create(Enchanted.location("familiars"), Enchanted.MOD_ID);
-	public static final Supplier<IForgeRegistry<FamiliarType<?, ?>>> REGISTRY = FAMILIAR_TYPES.makeRegistry(Generify.<FamiliarType<?, ?>>from(FamiliarType.class), RegistryBuilder::new);
+	private static final Map<ResourceLocation, FamiliarType<?, ?>> FAMILIAR_TYPES = new HashMap<>();
 
-	public static final RegistryObject<FamiliarType<?, ?>> CAT = FAMILIAR_TYPES.register("cat", CatFamiliarType::new);
-
+	public static final FamiliarType<?, ?> CAT = register("cat", new CatFamiliarType());
 
 
-	public static FamiliarType<?, ?> getByInput(EntityType<?> type) {
-		for(RegistryObject<FamiliarType<?, ?>> registryObject : FAMILIAR_TYPES.getEntries()) {
-			if(registryObject.get().getTypeIn() == type)
-				return registryObject.get();
+
+	/**
+	 * Register a {@link FamiliarType} to be used by Enchanted.
+	 */
+	public static <T extends TamableAnimal, C extends TamableAnimal> FamiliarType<T, C> register(ResourceLocation id, FamiliarType<T, C> type) {
+		FAMILIAR_TYPES.put(id, type);
+		return type;
+	}
+
+	public static <T extends TamableAnimal, C extends TamableAnimal> FamiliarType<T, C> register(String id, FamiliarType<T, C> type) {
+		return register(Enchanted.location(id), type);
+	}
+
+	public static FamiliarType<?, ?> get(ResourceLocation id) {
+		return FAMILIAR_TYPES.get(id);
+	}
+
+	public static FamiliarType<?, ?> getByInput(EntityType<?> typeIn) {
+		for(FamiliarType<?, ?> type : FAMILIAR_TYPES.values()) {
+			if(type.getTypeIn() == typeIn)
+				return type;
 		}
 		return null;
 	}

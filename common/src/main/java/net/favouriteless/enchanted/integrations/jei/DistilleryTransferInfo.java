831788f80be1d151046f669a58b22327c6f6054b
Favouriteless <favouriteless+github@favouriteless.dev>
2026-06-19 16:25:06 +0100

    JEI/EMI transfer bugfix

--- common/src/main/java/net/favouriteless/enchanted/integrations/jei/DistilleryTransferInfo.java
+++ common/src/main/java/net/favouriteless/enchanted/integrations/jei/DistilleryTransferInfo.java
@@ -1,5 +1,6 @@
 package net.favouriteless.enchanted.integrations.jei;
 
+import net.favouriteless.enchanted.common.init.registry.EItems;
 import net.favouriteless.enchanted.common.init.registry.EMenuTypes;
 import net.favouriteless.enchanted.common.menus.DistilleryMenu;
 import net.favouriteless.enchanted.common.recipes.DistillingRecipe;
@@ -7,6 +8,7 @@ import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.transfer.IRecipeTransferInfo;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,17 +21,23 @@ public class DistilleryTransferInfo implements IRecipeTransferInfo<DistilleryMen
     }
 
     @Override
-    public boolean canHandle(DistilleryMenu container, DistillingRecipe recipe) {
+    public boolean canHandle(DistilleryMenu menu, DistillingRecipe recipe) {
         return true;
     }
 
     @Override
-    public List<Slot> getRecipeSlots(DistilleryMenu container, DistillingRecipe recipe) {
-        return container.slots.subList(0, 3);
+    public List<Slot> getRecipeSlots(DistilleryMenu menu, DistillingRecipe recipe) {
+        int firstSlotIndex = 1;
+        for(ItemStack input : recipe.getItemsIn()) {
+            if(input.is(EItems.CLAY_JAR.get()))
+                firstSlotIndex = 0;
+        }
+
+        return menu.slots.subList(firstSlotIndex, 3);
     }
 
     @Override
-    public boolean requireCompleteSets(DistilleryMenu container, DistillingRecipe recipe) {
+    public boolean requireCompleteSets(DistilleryMenu menu, DistillingRecipe recipe) {
         return false;
     }
 
